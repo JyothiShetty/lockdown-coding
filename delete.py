498b38af8a296d15740b9efb01a064638b9afd3b
Jyothi B R <43513234+JyothiShetty@users.noreply.github.com>
2020-05-21 17:23:01 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,5 +1,5 @@
-rows = int(input("enter number of rows \n"))
-for i in range(0, rows + 1):
-    for j in range(rows - i, 0, -1):
-        print(j, end=' ')
+n = int(input("enter number of rows \n"))
+for i in range(0, n + 1):
+    for j in range(n - i, 0, -1):
+        print(j, end=" ")
 print(" ")
